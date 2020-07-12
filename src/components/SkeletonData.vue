3122d842ba14924e60e03c30407d72f747325ad9
Achmad Dzulfian <achmad.dzulfian7@gmail.com>
2020-07-12 16:58:16 +0700

    Update responsive skeleton data

--- src/components/SkeletonData.vue
+++ src/components/SkeletonData.vue
@@ -34,5 +34,47 @@
           </center>
         </p>
       </div>
+      <div class="content sm:hidden mx-auto">
+        <p>
+          <center>
+            <div class="px-5">
+              <div class="py-10">
+                <div class="py-3">
+                  <b-skeleton width="90%" active></b-skeleton>
+                </div>
+                <div class="py-3">
+                  <b-skeleton width="60%" active></b-skeleton>
+                </div>
+                <div class="py-3">
+                  <b-skeleton width="80%" active></b-skeleton>
+                </div>
+              </div>
+              <div class="pb-5 block">
+                <div class="w-full my-5 rounded-md mx-auto shadow-lg bg-white">
+                  <div class="py-5 px-4">
+                    <div class="py-2"><b-skeleton></b-skeleton></div>
+                    <div class="py-2"><b-skeleton width="50%"></b-skeleton></div>
+                    <div class="py-2"><b-skeleton></b-skeleton></div>
+                  </div>
+                </div>
+                <div class="w-full my-5 rounded-md mx-auto shadow-lg bg-white">
+                  <div class="py-5 px-4">
+                    <div class="py-2"><b-skeleton></b-skeleton></div>
+                    <div class="py-2"><b-skeleton width="50%"></b-skeleton></div>
+                    <div class="py-2"><b-skeleton></b-skeleton></div>
+                  </div>
+                </div>
+                <div class="w-full my-5 rounded-md mx-auto shadow-lg bg-white">
+                  <div class="py-5 px-4">
+                    <div class="py-2"><b-skeleton></b-skeleton></div>
+                    <div class="py-2"><b-skeleton width="50%"></b-skeleton></div>
+                    <div class="py-2"><b-skeleton></b-skeleton></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </center>
+        </p>
+      </div>
     </div>
 </template>
