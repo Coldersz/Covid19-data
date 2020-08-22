658b3dcd6b0e83900fef07254a2606a893eed704
Achmad Dzulfian <achmad.dzulfian7@gmail.com>
2020-08-22 11:24:55 +0700

    update: update theme manifest.json in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   pwa: {
+    themeColor: "#9F7AEA",
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'white',
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
       swSrc: "src/service-worker.js"
