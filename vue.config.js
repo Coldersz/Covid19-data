8ecab6dfbf57f780276bad4c873a69eaca4fabad
Achmad Dzulfian <achmad.dzulfian7@gmail.com>
2020-08-28 15:43:49 +0700

    update: configure vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,19 @@
 module.exports = {
   pwa: {
-    manifestOptions: {
-      background_color: "#FFFFFF"
-    },
-    themeColor: "#9F7AEA",
+    manifestPath: 'manifest.json',
+    themeColor: '#FFFFFF',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'white',
-    workboxPluginMode: "InjectManifest",
+    workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: "src/service-worker.js"
+      swSrc: 'src/service-worker.js'
+    },
+    iconPaths: {
+      favicon32: 'img/icons/logo-32x32.png',
+      favicon16: 'img/icons/logo-16x16.png',
+      appleTouchIcon: 'img/icons/logo.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/logo.png'
     }
   }
 };
